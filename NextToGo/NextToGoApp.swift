c3930d0c0dcfe6e3671c03adba3e7c69be81f288
rhyg <rhysgeary@gmail.com>
2023-06-24 13:16:28 +1000

    Initial Commit

---
+++ NextToGo/NextToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoApp.swift
+//  NextToGo
+//
+//  Created by Rhys Geary on 24/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
